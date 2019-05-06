66396f76c2082bd0006c037e321022f974dd691e
OceanTheSail <1909519433@qq.com>
2019-05-06 17:26:42 +0800

    上传MyTerrains

---
+++ MyTerrains/Assets/MyControl.cs
@@ -0,0 +1,44 @@
+﻿// 柯博文老師  www.powenko.com
+using UnityEngine;
+using System.Collections;
+
+public class MyControl : MonoBehaviour {
+	/*
+	public  AnimationClip Walk;
+	public float WalkSpeed=3.0f;
+	public  AnimationClip Run;
+*/
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		float Speed;
+		Speed=WalkSpeed;
+	*/
+		/*
+		float WalkSpeed=1.0f;
+		float x=Input.GetAxis("Vertical")*WalkSpeed*Time.deltaTime;
+		float y=Input.GetAxis("Horizontal")*WalkSpeed*Time.deltaTime;
+		Vector3 t1=gameObject.transform.position;
+		t1.x=t1.x+x;
+		t1.z=t1.z+y;
+
+		gameObject.transform.position=t1;
+*/
+
+		if (Input.GetButton("Jump")) {
+			GetComponent<Animation>().CrossFade ("Attack");
+		}else if (Input.GetButton("Fire1")) {
+
+			GetComponent<Animation>().CrossFade ("Jump");
+		}else{
+
+		   if (Input.GetAxis("Vertical") > 0.2f){
+		    	GetComponent<Animation>().CrossFade ("Walk");
+		   }else{
+		    	GetComponent<Animation>().CrossFade ("idle");
+		   }
+		}
+	
+	}
+}
---
+++ MyTerrains/Assets/MyControl_move.cs
@@ -0,0 +1,33 @@
+﻿// 柯博文老師  www.powenko.com
+using UnityEngine;
+using System.Collections;
+public class MyControl_move : MonoBehaviour {
+	
+	
+	void Update () {
+		
+		// 人物位置控制
+		float WalkSpeed=1.0f;
+		float x=Input.GetAxis("Vertical")*WalkSpeed*Time.deltaTime;
+		float y=Input.GetAxis("Horizontal")*WalkSpeed*Time.deltaTime;
+		Vector3 t1=gameObject.transform.position;
+		t1.x=t1.x+x;
+		t1.z=t1.z+y;
+		gameObject.transform.position=t1;
+		
+		// 人物動作控制
+		if (Input.GetButton("Jump")) {
+			GetComponent<Animation>().CrossFade ("Attack");
+		}else if (Input.GetButton("Fire1")) {
+			GetComponent<Animation>().CrossFade ("Jump");
+		}else{
+			if (Input.GetAxis("Vertical") > 0.2f){
+				GetComponent<Animation>().CrossFade ("Walk");
+			}else{
+				GetComponent<Animation>().CrossFade ("idle");
+			}
+		}
+		
+	}
+}
+
